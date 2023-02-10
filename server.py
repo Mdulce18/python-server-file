d3e805bc73cca5c17525faa2487fdecf50805fda
Mdulce18 <42708203+Mdulce18@users.noreply.github.com>
2023-02-10 14:01:51 -0300

    Update server.py
    aded differents paths for upload and downloads

--- server.py
+++ server.py
@@ -1,47 +1,43 @@
 import os
 
 from flask import Flask, request, abort, jsonify, send_from_directory
+from werkzeug.utils import secure_filename
 
+UPLOAD_DIRECTORY = "./files/"
+DOWNLOAD_DIRECTORY = "./payloads/"
 
-UPLOAD_DIRECTORY = "/files/"
+def create_directories(directories):
+    for d in directories:
+        if not os.path.exists(d):
+            os.makedirs(d)
 
-if not os.path.exists(UPLOAD_DIRECTORY):
-    os.makedirs(UPLOAD_DIRECTORY)
-
-
+create_directories(["./files/","./payloads/"])
 api = Flask(__name__)
 
 
-@api.route("/files")
+@api.route("/payloads")
 def list_files():
     """Endpoint to list files on the server."""
     files = []
-    for filename in os.listdir(UPLOAD_DIRECTORY):
-        path = os.path.join(UPLOAD_DIRECTORY, filename)
+    for filename in os.listdir(DOWNLOAD_DIRECTORY):
+        path = os.path.join(DOWNLOAD_DIRECTORY, filename)
         if os.path.isfile(path):
             files.append(filename)
     return jsonify(files)
 
 
-@api.route("/files/<path:path>")
+@api.route("/payloads/<path:path>")
 def get_file(path):
     """Download a file."""
-    return send_from_directory(UPLOAD_DIRECTORY, path, as_attachment=True)
+    return send_from_directory(DOWNLOAD_DIRECTORY, path, as_attachment=True)
 
 
-@api.route("/files/<filename>", methods=["POST"])
-def post_file(filename):
-    """Upload a file."""
-
-    if "/" in filename:
-        # Return 400 BAD REQUEST
-        abort(400, "no subdirectories allowed")
-
-    with open(os.path.join(UPLOAD_DIRECTORY, filename), "wb") as fp:
-        fp.write(request.data)
-
-    # Return 201 CREATED
-    return "", 201
+@api.route("/files_up", methods=["GET","POST"])
+def post_file():
+    if request.method == 'POST':
+      f = request.files['file']
+      f.save(os.path.join(UPLOAD_DIRECTORY,secure_filename(f.filename)))
+      return 'file uploaded successfully', 201
 
 
 if __name__ == "__main__":
